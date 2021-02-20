0a480856b499bf00479799ff883ac06c2349652a
szypkiwonsz <kacpersawicki321@gmail.com>
2021-02-20 23:02:58 +0100

    Add djoser.urls.jwt path to urlpatterns

--- backend/work_hours/urls.py
+++ backend/work_hours/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt'))
 ]
